9206fe7642128cdc2582cf0d69857e3eb3a87c58
fuhaidalsubea <38795160+fuhaidalsubea@users.noreply.github.com>
2018-04-27 22:40:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,17 +3,14 @@ public class Main {
 	public static void main(String[] args) {
 		DQueue<Character>[] dq = Expr.readExpressions("input/test.txt");
 		for (int i = 0; i < dq.length && dq[i] != null; i++) {
-			int size = dq[i].length();
-			// import commnt about for loop
-			for (int j = 0; j < size; j++) {
+			int size = dq[i].length();			
 				char c = dq[i].removeFirst();
 				dq[i].addLast(c);
 				System.out.print(c);
-			}
 			System.out.println("= " + Expr.evaluateExpression(dq, i));
 			System.out.println("Reversible: " + Expr.reversible(dq, i) + "\n");
 		}
 	}
-	System.out.println("fuhaidalsubea")
+	System.out.println("fuhaidalsubea");
 
 }
