b1a0df9a5ccf62ae3a8f3d43f19674e0451a710e
fuhaidalsubea <38795160+fuhaidalsubea@users.noreply.github.com>
2018-04-27 22:16:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		DQueue<Character>[] dq = Expr.readExpressions("input/test.txt");
+		for (int i = 0; i < dq.length && dq[i] != null; i++) {
+			int size = dq[i].length();
+			for (int j = 0; j < size; j++) {
+				char c = dq[i].removeFirst();
+				dq[i].addLast(c);
+				System.out.print(c);
+			}
+			System.out.println("= " + Expr.evaluateExpression(dq, i));
+			System.out.println("Reversible: " + Expr.reversible(dq, i) + "\n");
+		}
+	}
+
+}
